5791f1263c2597f83cc4dc701c97d2724501f9e6
ellatom <ella271@gmail.com>
2025-05-21 15:54:43 +0300

    Updated url to test docker

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,9 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  pageLoadTimeout: 120000,
   e2e: {
-    baseUrl: 'https://www.imdb.com/',
+    baseUrl: 'https://example.cypress.io/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     }
